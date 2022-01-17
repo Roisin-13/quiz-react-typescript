dabcce32278150d1ad9db54f9c6356ca6244e366
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-01-17 12:43:42 +0000

    adding elements to app for quiz to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import QuestionCard from './components/QuestionCard';
+
 
 function App() {
+  const startQuiz = async () => {}
+  
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {}
+  
+const nextQuestion = () => {}
+
   return (
     <div className="App">
-      Quiz
+      <h1>React-Typescript Quiz</h1>
+      <button className="start" onClick={startQuiz}>start</button>
+      <p className="score">Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard/>
+      <button className="next" onClick={nextQuestion}>next question</button>
     </div>
   );
 }
