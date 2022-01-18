905b675ca9f4c9424b1b29090a0690cb714bd6b1
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-01-18 12:28:32 +0000

    adding endpoint to interact with quiz site

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchQuizQuestions } from './API';
 
 import QuestionCard from './components/QuestionCard';
 
+import { Difficulty } from './API';
+
+const TOTAL_QUESTIONS = 10;
+
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnsers] = useState([]);
+  const [score, seetScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+
   const startQuiz = async () => {}
   
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {}
   
-const nextQuestion = () => {}
+  const nextQuestion = () => {}
 
   return (
     <div className="App">
@@ -16,7 +30,14 @@ const nextQuestion = () => {}
       <button className="start" onClick={startQuiz}>start</button>
       <p className="score">Score:</p>
       <p>Loading Questions...</p>
-      <QuestionCard/>
+      {/* <QuestionCard
+        questionNo={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answer}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
       <button className="next" onClick={nextQuestion}>next question</button>
     </div>
   );
